725018e7ee802e1215219b3848c6435449018b36
Colin <colinelsinga@gmail.com>
2021-02-09 02:13:50 +0000

    implement product specs test

---
+++ app/models/product_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "checks that name isn't missing" do
+      @category = Category.create(name: "Vehicle")
+      @product = Product.create(price: 33400, quantity: 1, category: @category)
+      expect(@product.errors.full_messages).to include "Name can't be blank"
+    end
+
+    it "checks that price isn't missing" do
+      @category = Category.create(name: "Vehicle")
+      @product = Product.create(name: "Mazda", quantity: 2, category: @category)
+      expect(@product.errors.full_messages).to include "Price can't be blank"
+    end
+
+    it "checks that quanity isn't missing" do
+      @category = Category.create(name: "Vehicle")
+      @product = Product.create(name: "Audi", price: 53000, category: @category)
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
+
+    it "checks that category isn't missing" do
+      @category = Category.create(name: "Vehicle")
+      @product = Product.create(name: "Kia", price: 13000, quantity: 1)
+      expect(@product.errors.full_messages).to include "Category can't be blank"
+    end
+  end
+end
